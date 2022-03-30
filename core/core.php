07a77c6d334e4078c01d10c386cd32aac7e5458b
getfeatherapp <hi@getfeather.app>
2022-03-30 18:11:15 +0100

    Update core.php

--- core/core.php
+++ core/core.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined( "FEATHER_INIT" ) ) die();
 /**
- * Core functions that output 
+ * Core functions that output
  * in the document <head>
  */
 if ( ! function_exists( "feather_head" ) )
@@ -11,13 +11,13 @@ if ( ! function_exists( "feather_head" ) )
 			<meta name='generator' content='Feather ".FEATHER_VERSION."' />
 			<link href='".get_core_css_dir()."emoji.min.css' rel='stylesheet' async='async' />
 			<link href='".get_core_css_dir()."animate.min.css' rel='stylesheet' async='async' />
-			
+
 			<script src='".get_core_js_dir()."jquery.min.js' defer='defer'></script>
 		";
 	}
 }
 /**
- * Core functions that output 
+ * Core functions that output
  * before the closing </body>
  */
 if ( ! function_exists( "feather_footer" ) )
@@ -25,9 +25,9 @@ if ( ! function_exists( "feather_footer" ) )
 	function feather_footer()
 	{
 		/**
-		 * Automatically execute Sitemap 
-		 * rebuild without requiring the 
-		 * user needing to setup a cronjob.
+		 * Automatically execute Sitemap
+		 * rebuild without requiring the
+		 * user to setup a cronjob.
 		 *
 		 * Requires site visits to trigger.
 		 *
@@ -41,8 +41,8 @@ if ( ! function_exists( "feather_footer" ) )
 		else
 		{
 			$time = date( "H:i:s" );
-	
-			if ( $time > "08:00:00" && $time < "12:00:00"  ) 
+
+			if ( $time > "08:00:00" && $time < "12:00:00"  )
 			{
 				echo "<img src='".get_base_url()."/index.php?sitemap=true' width='1px' height='1px' />";
 			}
